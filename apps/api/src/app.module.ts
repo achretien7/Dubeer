962a3065baeae6981f37825bf5b8ef458af3b93b
Alexis Chretien <alexis.chretien1@outlook.com>
2026-02-06 15:04:47 +0100

    fix: use default export for TypeORM data source

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Venue } from './venues/entities/venue.entity';
-import { AppDataSource } from './database/data-source';
+import AppDataSource from './database/data-source';
 import { VenuesModule } from './venues/venues.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
